898de38f4f0612203153ab012bc4359fc39fc79c
Nishesh Chakrapani <nishesh_chakrapani@outlook.com>
2017-07-26 14:08:09 -0700

    Tower of Hanoi solving code

---
+++ src/TowerOfHanoi.java
@@ -0,0 +1,22 @@
+/**
+ * Created by nishu on 2/12/2017.
+ */
+public class TowerOfHanoi {
+    public static int numMoves = 0;
+    public static void main(String[] args) {
+        move(10, 0, 1, 2);
+        System.out.println("Total moves: " + numMoves);
+    }
+
+    public static void move(int topDisk, int from, int using, int to) {
+        if (topDisk == 1) {
+            System.out.println("Move disk 1 from " + from + " to " + to);
+            numMoves++;
+        } else {
+            move(topDisk-1, from, to, using);
+            System.out.println("Move disk " + topDisk + " from " + from + " to " + to);
+            move(topDisk-1, using, from, to);
+            numMoves++;
+        }
+    }
+}
