fecb723f93ebd96de494249213f36de52439c308
Nishesh Chakrapani <nishesh_chakrapani@outlook.com>
2017-10-20 09:37:01 -0700

    package private

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -2,13 +2,13 @@
  * Created by nishu on 2/12/2017.
  */
 public class TowerOfHanoi {
-    public static int numMoves = 0;
+    private static int numMoves = 0;
     public static void main(String[] args) {
         move(10, 0, 1, 2);
         System.out.println("Total moves: " + numMoves);
     }
 
-    public static void move(int topDisk, int from, int using, int to) {
+    private static void move(int topDisk, int from, int using, int to) {
         if (topDisk == 1) {
             System.out.println("Move disk 1 from " + from + " to " + to);
             numMoves++;
